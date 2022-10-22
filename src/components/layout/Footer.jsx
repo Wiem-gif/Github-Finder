0a6a1ee0d63af2eb2bf8ef449eb27be2b0e49bc8
Wiem <wiemb55@gmail.com>
2022-10-22 06:12:38 +0100

    first commit

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Footer() {
+    const footerYear = new Date().getFullYear();
+  return (
+   <footer className="footer p-10 bg-gray-700 text-primary-contentfooter-center">
+    <div>
+        <svg>
+            
+        </svg>
+    </div>
+   </footer>
+  )
+}
